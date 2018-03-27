d8133cda85e1cf57c183d3cb7f7c6b2d724ceb66
calvarezs <31164535+calvarezs@users.noreply.github.com>
2018-03-26 22:27:24 -0300

    Esqueleto inicial programa

---
+++ Lab1_Orga_19560440-1/include/Includes.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+//Definicion de librerias ANSI utilizadas
+#include <stdio.h>
+#include <stdlib.h>  
+#include <string.h>
+
+//Definicion de tipo de dato bool
+#define TRUE 1
+#define FALSE 0  
+#define LARGO_NOMBRE_COMPLETO 100
+#define LARGO_NOMBRE 25
+#define LARGO_AUXILIAR 3
+
+//Definicion de librerias propias utilizadas
+#include "ImprimirResultados.h"
+
+#endif
